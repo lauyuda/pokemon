e396788631831018a399251b81f72c4481512fd9
Lau Yu Da <lauyuda@gmail.com>
2021-09-25 09:20:40 +0800

    debug

--- src/components/typecard.jsx
+++ src/components/typecard.jsx
@@ -45,31 +45,32 @@ export const TypeCard = ({ types, isSeeThrough = false, children }) => {
     };
 
     const toColour = {
-        normal: "from-gray-400",
-        poison: "from-purple-900",
-        grass: "from-green-500",
-        fire: "from-red-400",
-        water: "from-blue-600",
-        bug: "from-green-300",
-        flying: "from-purple-400",
-        steel: "from-gray-500",
-        dragon: "from-purple-600",
-        ice: "from-blue-300",
-        fighting: "from-yellow-900",
-        rock: "from-yellow-800",
-        ground: "from-yellow-600",
-        dark: "from-gray-900",
-        ghost: "from-indigo-900",
-        psychic: "from-pink-500",
-        electric: "from-yellow-400",
-        fairy: "from-pink-300",
+        normal: "to-gray-400",
+        poison: "to-purple-900",
+        grass: "to-green-500",
+        fire: "to-red-400",
+        water: "to-blue-600",
+        bug: "to-green-300",
+        flying: "to-purple-400",
+        steel: "to-gray-500",
+        dragon: "to-purple-600",
+        ice: "to-blue-300",
+        fighting: "to-yellow-900",
+        rock: "to-yellow-800",
+        ground: "to-yellow-600",
+        dark: "to-gray-900",
+        ghost: "to-indigo-900",
+        psychic: "to-pink-500",
+        electric: "to-yellow-400",
+        fairy: "to-pink-300",
     };
 
     function colourPicker(prefix, color) {
         return `${prefix}-${typeColour[color]}`
     }
     const cardFromColour2 = fromColor[types[0].type.name];
-    const cardToColour2 = types.length === 1 ? colourPicker("to", types[0].type.name) : colourPicker("to", types[1].type.name);
+    const cardToColour2 = types.length === 1 ? toColour[types[0].type.name] : toColour[types[1].type.name];
+    const cardToColour3 = types.length === 1 ? colourPicker("to", types[0].type.name) : colourPicker("to", types[1].type.name);
 
     // Assuming a pokemon can have 2 different types max
     // if only 1 type, both from and to gradient colors will be the same
@@ -88,7 +89,7 @@ export const TypeCard = ({ types, isSeeThrough = false, children }) => {
         <div className="relative w-full my-1">
             {/* <div className={`absolute inset-0 w-full rounded-lg bg-gradient-to-b ${isSeeThrough && "opacity-50"} ` + gradientColours }></div> */}
             <div className={`absolute inset-0 w-full rounded-lg bg-gradient-to-b ${cardToColour2} ` + cardFromColour2}></div>
-            <div className={"relative" + gradientColours}>
+            <div className={"relative" + gradientColours + cardToColour3}>
                 {children}
             </div>
         </div>
