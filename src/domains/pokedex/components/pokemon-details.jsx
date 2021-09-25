f26d4b7a8ed37e2a7e2858c9277f644e11644046
Lau Yu Da <lauyuda@gmail.com>
2021-09-25 09:46:12 +0800

    debug

--- src/domains/pokedex/components/pokemon-details.jsx
+++ src/domains/pokedex/components/pokemon-details.jsx
@@ -18,10 +18,6 @@ export const PokemonDetails = ({ pokemonName }) => {
 
   let colour = "";
   !isLoading && (colour = typeColour(data.types[0].type.name));
-  let borderColour = ""
-  useEffect(() => {
-    borderColour = typeColour(data.types[0].type.name)
-  }, [isLoading, colour])
 
   return (
     <div>
@@ -30,7 +26,7 @@ export const PokemonDetails = ({ pokemonName }) => {
           <div className="fixed w-2/5 px-10 mb-10 mt-5">
             <div className="relative">
               <img src={data.sprites.other.dream_world.front_default} className="w-3/5 relative z-10" alt="" />
-              <div className={`flex flex-col border-4 absolute top-10 bottom-10 left-10 right-10 z-0 items-start justify-center border-${colour} border-${borderColour}`} style={{ 'paddingLeft': "55%" }}>
+              <div className={`flex flex-col border-4 absolute top-10 bottom-10 left-10 right-10 z-0 items-start justify-center border-${colour} ` +colour+" border-"+colour} style={{ 'paddingLeft': "55%" }}>
                 <p className="text-gray-500 md:text-sm lg:text-xl mt-3">#{data.id}</p>
                 <div className="flex -center gap-2">
                   <p className="capitalize text-gray-800 md:text-l lg:text-3xl font-semibold mb-5">{pokemonName}</p>
