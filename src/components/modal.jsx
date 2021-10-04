623c5c351cffd0998b746a90c62f800cb74e4b93
Lau Yu Da <lauyuda@gmail.com>
2021-10-04 23:48:33 +0800

    update close modal on click bg

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -3,8 +3,8 @@ import { TrashIcon } from "@heroicons/react/outline";
 
 export const Modal = ({ onCancel, onRelease, pokemonName }) => {
     return (
-        <div className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover" id="modal-id">
-            <div className="absolute bg-black opacity-80 inset-0 z-0"></div>
+        <div className="min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover" id="modal-id">
+            <div className="absolute bg-black opacity-80 inset-0 z-0" onClick={onCancel}></div>
             <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg bg-white ">
                 <div className="">
                     <div className="text-center p-5 flex-auto justify-center">
@@ -13,7 +13,7 @@ export const Modal = ({ onCancel, onRelease, pokemonName }) => {
                         <p className="text-sm text-gray-500 px-8">Do you want to release <span className="capitalize">{pokemonName}</span>?
                             <br /> This process cannot be undone.</p>
                     </div>
-                    <div className="p-3  mt-2 text-center space-x-4 md:block">
+                    <div className="p-3 mt-2 text-center space-x-4 md:block">
                         <button className="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100" onClick={onCancel}>
                             Cancel
                         </button>
